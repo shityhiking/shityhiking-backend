a4be44492141303367c464c6898b2e06ea1ae59a
s8sg <swarvanusg@gmail.com>
2018-10-23 12:20:52 +0900

    Add upcoming meetup event get
    Signed-off-by: s8sg <swarvanusg@gmail.com>

--- meetup-client/handler.py
+++ meetup-client/handler.py
@@ -2,42 +2,54 @@ import requests
 import sys
 from get_docker_secret import get_docker_secret
 import json
+import os
 
-#consumer_id = ""
-#consumer-secret = ""
+# consumer_id = ""
+# consumer-secret = ""
+
 refresh_token = ""
 
 
+def get_secret_mount_path():
+    secrets_mount_path = os.getenv("secrets_mount_path")
+    if secrets_mount_path is None or secrets_mount_path == "":
+        secrets_mount_path = "/var/run/secrets/"
+    return secrets_mount_path
+
+
 def set_global():
-#    global consumer_id
-#    global consumer-secret
+    # global consumer_id
+    # global consumer-secret
     global refresh_token
 
 #    consumer_id = os.getenv("consumer_id")
 #    if consumer_id == None:
 #        print("consumer_id is not set")
-#        sys.exit(1) 
-    refresh_token = get_docker_secret("refresh_token", safe=True, secrets_dir="/var/openfaas/secrets/", envvar=True) 
-    if refresh_token == None:
+#        sys.exit(1)
+    secrets_mount_path = get_secret_mount_path()
+    refresh_token = get_docker_secret("refresh_token", safe=True,
+                                      secrets_dir=secrets_mount_path)
+    if refresh_token is None:
         print("refresh_token is not set")
-        sys.exit(1) 
-#    consumer-secret = get_docker_secret("consumer-secret", safe=True, secrets_dir="/var/openfaas/secrets/")
+        sys.exit(1)
+#    consumer-secret = get_docker_secret("consumer-secret", safe=True,
+#                                        secrets_dir="/var/openfaas/secrets/")
 #    if consumer-secret == None:
 #        print("consumer-secret is not set")
-#        sys.exit(1) 
+#        sys.exit(1)
 
 
 def handle_upcoming_events():
-    payload = {'group_urlname': 'SHITY-Hiking-and-Outdoors', 'access_token': 'refresh_token'}
-    r = requests.get('https://api.meetup.com/2/events', params=payload)
-    r.raise_for_status()
-    data = r.json()
-    
+    payload = {'group_urlname': 'SHITY-Hiking-and-Outdoors',
+               'access_token': 'refresh_token'}
+    resp = requests.get('https://api.meetup.com/2/events', params=payload)
+    resp.raise_for_status()
+    data = resp.json()
     result = {}
     result['events'] = data['results']
     result['count'] = data['meta']['count']
 
-    return json.dumps(result) 
+    return json.dumps(result)
 
 
 def handle(req):
@@ -47,9 +59,9 @@ def handle(req):
     """
     handler = None
     if req == "upcoming-events":
-         handler = handle_upcoming_events
+        handler = handle_upcoming_events
 
-    if handler != None:
+    if handler is not None:
         set_global()
         return handler()
 
